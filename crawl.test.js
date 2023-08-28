b3dc52734e9125d76d3b76d268376a5248f8da1f
aryans98 <aryansindhav@gmail.com>
2023-08-28 18:08:39 +0530

    updated crawl.test.js file

--- crawl.test.js
+++ crawl.test.js
@@ -2,60 +2,60 @@ const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL protocol', () => {
-  const input = 'https://blog.boot.dev/path'
+  const input = 'https://aryan-sindhav.netlify.app/path'
   const actual = normalizeURL(input)
-  const expected = 'blog.boot.dev/path'
+  const expected = 'aryan-sindhav.netlify.app/path'
   expect(actual).toEqual(expected)
 })
 
 test('normalizeURL slash', () => {
-  const input = 'https://blog.boot.dev/path/'
+  const input = 'https://aryan-sindhav.netlify.app/path/'
   const actual = normalizeURL(input)
-  const expected = 'blog.boot.dev/path'
+  const expected = 'aryan-sindhav.netlify.app/path'
   expect(actual).toEqual(expected)
 })
 
 test('normalizeURL capitals', () => {
-  const input = 'https://BLOG.boot.dev/path'
+  const input = 'https://Aryan-sindhav.netlify.app/path'
   const actual = normalizeURL(input)
-  const expected = 'blog.boot.dev/path'
+  const expected = 'aryan-sindhav.netlify.app/path'
   expect(actual).toEqual(expected)
 })
 
 test('normalizeURL http', () => {
-  const input = 'http://BLOG.boot.dev/path'
+  const input = 'http://Aryan-sindhav.netlify.app/path'
   const actual = normalizeURL(input)
-  const expected = 'blog.boot.dev/path'
+  const expected = 'aryan-sindhav.netlify.app/path'
   expect(actual).toEqual(expected)
 })
 
 test('getURLsFromHTML absolute', () => {
-  const inputURL = 'https://blog.boot.dev'
-  const inputBody = '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>'
+  const inputURL = 'https://aryan-sindhav.netlify.app'
+  const inputBody = '<html><body><a href="https://aryan-sindhav.netlify.app"><span>Aryan-sindhav.netlify.app></span></a></body></html>'
   const actual = getURLsFromHTML(inputBody, inputURL)
-  const expected = [ 'https://blog.boot.dev/' ]
+  const expected = [ 'https://aryan-sindhav.netlify.app/' ]
   expect(actual).toEqual(expected)
 })
 
 test('getURLsFromHTML relative', () => {
-  const inputURL = 'https://blog.boot.dev'
-  const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>'
+  const inputURL = 'https://aryan-sindhav.netlify.app'
+  const inputBody = '<html><body><a href="/path/one"><span>Aryan-sindhav.netlify.app></span></a></body></html>'
   const actual = getURLsFromHTML(inputBody, inputURL)
-  const expected = [ 'https://blog.boot.dev/path/one' ]
+  const expected = [ 'https://aryan-sindhav.netlify.app/path/one' ]
   expect(actual).toEqual(expected)
 })
 
 test('getURLsFromHTML both', () => {
-  const inputURL = 'https://blog.boot.dev'
-  const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>'
+  const inputURL = 'https://aryan-sindhav.netlify.app'
+  const inputBody = '<html><body><a href="/path/one"><span>Aryan-sindhav.netlify.app></span></a><a href="https://other.com/path/one"><span>Aryan-sindhav.netlify.app></span></a></body></html>'
   const actual = getURLsFromHTML(inputBody, inputURL)
-  const expected = [ 'https://blog.boot.dev/path/one', 'https://other.com/path/one' ]
+  const expected = [ 'https://aryan-sindhav.netlify.app/path/one', 'https://other.com/path/one' ]
   expect(actual).toEqual(expected)
 })
 
 test('getURLsFromHTML handle error', () => {
-  const inputURL = 'https://blog.boot.dev'
-  const inputBody = '<html><body><a href="path/one"><span>Boot.dev></span></a></body></html>'
+  const inputURL = 'https://aryan-sindhav.netlify.app'
+  const inputBody = '<html><body><a href="path/one"><span>Aryan-sindhav.netlify.app></span></a></body></html>'
   const actual = getURLsFromHTML(inputBody, inputURL)
   const expected = [ ]
   expect(actual).toEqual(expected)
